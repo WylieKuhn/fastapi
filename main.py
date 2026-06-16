0c6837c82dd62502f4ccd4dcb587a3b8194681d5
Wylie Kuhn <wyliekuhnaero@gmail.com>
2026-06-16 02:31:02 -0400

    Test Deployment

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/status")
+async def root():
+    return {"status": "Rockin", "message": "Hamlin 4 Champ"}
